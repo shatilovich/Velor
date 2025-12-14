fda51f398fa4bac4eb05a118decf759f2972e750
Roma <roma@yandex.ru>
2025-12-14 14:06:45 +0500

    Initial Commit

---
+++ Velor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Velor
+//
+//  Created by Shatilovich.R on 14.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Velor/VelorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelorApp.swift
+//  Velor
+//
+//  Created by Shatilovich.R on 14.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct VelorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
